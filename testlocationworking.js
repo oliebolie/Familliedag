bb1da2e0dd9b8695f7d76832736c0283e4a4c337
oliebolie <151738254+oliebolie@users.noreply.github.com>
2024-08-08 14:11:00 +0200

    added the location tester to see if its works

---
+++ testlocationworking.js
@@ -0,0 +1,20 @@
+
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+document.getElementById('testlocationworking').addEventListener('click', function(){
+    const x = document.getElementById("locationworking");
+    getLocation();
+})
