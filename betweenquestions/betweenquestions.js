c7c69ea57a84316d29fae3f9c2ad09016a9ac6f3
Ole Zuijderduijn <olezuijderduijn@gmail.com>
2024-08-08 15:18:31 +0200

    Got the location working so it can detect when you are eless then 40 meters from a point.

---
+++ betweenquestions/betweenquestions.js
@@ -0,0 +1,55 @@
+//the hard part...
+//we need to track the location of the user and compare it to the nextlocation cookie.
+//If the difference is less then 40 we can ask the question
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+let distance = 1000
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(showPosition);
+    } else {
+      console.log('No data sorry, gelocation is not supported');
+    }
+  }
+  
+  function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude)
+
+    //d=√((x_2-x_1)²+(y_2-y_1)²)
+    let lat1 = position.coords.latitude;
+    let long1 = position.coords.longitude;
+
+    let nextlocation = getCookie("NextLocation");
+
+    let lat2 = nextlocation.split("-")[0];
+    let long2 = nextlocation.split("-")[1];
+
+    //latitude and longitude will be getting from the cookie
+
+    distance = Math.sqrt(Math.pow(lat2 - lat1 ,2) + Math.pow(long2 - long1, 2)) * 111320; // convert to meters
+    console.log(distance)
+
+    if (distance < 40) {
+      // activate here!
+      console.log("You're within 40 meters! Ask the question...");
+      // add your code to ask the question here
+    }
+  }
+
+getLocation()
--- startgame.js
+++ startgame.js
@@ -3,7 +3,9 @@
 document.getElementById('startgame').addEventListener('click', function(){
     setCookie("Startingtime", new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), 1)
     setCookie("NextLocation", "51.58096516472835-4.784654191057734", 1)
-    
+    setCookie("Level", 1, 1)
+    //here it opens waiting for location .html
+    window.location.href = "/betweenquestions/waitingforlocation.html";
 
 })
 
